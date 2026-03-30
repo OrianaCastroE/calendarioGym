373848c783ca974e1833e5bf3b5884ef77dd3f7d
Emi1891 <emilianoreyesmanya@gmail.com>
2026-03-30 10:23:59 -0300

    RED: valid user sign up

---
+++ DarkKitchen/DarkKitchen.API/Controllers/UserController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DarkKitchen.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+}
---
+++ DarkKitchen/DarkKitchen.Tests/Controllers/UserControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace DarkKitchen.Tests.Controllers;
+
+[TestClass]
+public class UserControllerTest
+{
+    private readonly string validEmail = "valid@email.com";
+    private readonly string password = "ValidPassword123!";
+    private readonly string validPhone = "+59899123123";
+    private Mock<IUserService>? userServiceMock;
+    private UserController? userController;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        userServiceMock = new Mock<IUserService>();
+        userController = new UserController(userServiceMock.Object);
+    }
+
+    [TestMethod]
+    public void UserSignUp_ValidUserWithoutRole_UserRegisteredAsClient()
+    {
+        userServiceMock.Setup(s => s.UserExists().Returns(false));
+        var signUpDto = new SignUpDto()
+        {
+            Name = "Name",
+            Surname = "Surname",
+            Email = validEmail,
+            Phone = validPhone,
+            Password = password
+        };
+        var result = userController.SignUp(signUpDto);
+        var resultObj = result as ObjectResult;
+
+        Assert.IsNotNull(resultObj);
+        Assert.AreEqual(201, resultObj.StatusCode);
+    }
+}
