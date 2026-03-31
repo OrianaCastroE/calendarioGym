27a797da9bbca72db23aa291e29017b3c99df3a0
Juanicroquis <jc286049@fi365.ort.edu.uy>
2026-03-30 22:23:36 -0300

    CreateOrder - GREEN

---
+++ DarkKitchen/DarkKitchen.API/Controllers/OrdersController.cs
@@ -0,0 +1,26 @@
+﻿using Domain.DTOs.OrderDTOs;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DarkKitchen.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrdersController(IOrderService orderService) : ControllerBase
+{
+    private readonly IOrderService _orderService = orderService;
+
+    [HttpPost]
+    public IActionResult CreateOrder([FromBody] OrderDto newOrder)
+    {
+        try
+        {
+            var order = _orderService.CreateOrder(newOrder);
+            return Created("", order);
+        }
+        catch(Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
