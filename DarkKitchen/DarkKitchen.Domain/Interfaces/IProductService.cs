b2d8cb5197f2ae3431534c8fd04195367b19700b
Emi1891 <emilianoreyesmanya@gmail.com>
2026-04-04 15:21:41 -0300

    Changed dto used so it implements new UpdateProductDto

--- DarkKitchen/DarkKitchen.Domain/Interfaces/IProductService.cs
+++ DarkKitchen/DarkKitchen.Domain/Interfaces/IProductService.cs
@@ -1,12 +1,13 @@
-﻿using DarkKitchen.Models.ProductDTOs;
+﻿using DarkKitchen.Models.DateDTOs;
+using DarkKitchen.Models.ProductDTOs;
 
 namespace DarkKitchen.Domain.Interfaces;
 
 public interface IProductService
 {
     public void CreateProduct(CreateProductDto newProduct);
-    public void UpdateProduct(ProductDto updatedProduct);
-    public IEnumerable<ProductDto> GetProducts(string? productLine, List<string>? categories, string? name);
-    public IEnumerable<ProductDto> GetMostRequestedProducts();
+    public void UpdateProduct(UpdateProductDto updatedProduct);
+    public IEnumerable<UpdateProductDto> GetProducts(string? productLine, List<string>? categories, string? name);
+    public IEnumerable<UpdateProductDto> GetMostRequestedProducts(DateRangeDto dates);
     public void UpdateStatus(int id, ProductStatusDto status);
 }
