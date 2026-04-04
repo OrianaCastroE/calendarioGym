80340e58d2e389d9058e0b161497f677c866e663
Emi1891 <emilianoreyesmanya@gmail.com>
2026-04-03 22:58:22 -0300

    Created custom exceptions  to use in ExeptionFilter.cs

---
+++ DarkKitchen/Domain/Exceptions/AccessDeniedException.cs
@@ -0,0 +1,5 @@
+namespace Domain.Exceptions;
+
+public class AccessDeniedException(string message) : Exception(message)
+{
+}
---
+++ DarkKitchen/Domain/Exceptions/InvalidInputException.cs
@@ -0,0 +1,5 @@
+namespace Domain.Exceptions;
+
+public class InvalidInputException(string message) : Exception(message)
+{
+}
