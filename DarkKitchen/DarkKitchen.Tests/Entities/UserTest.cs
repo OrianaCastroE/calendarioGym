6e7d40fcbc0972c42019841fa2056246bc6be731
Emi1891 <emilianoreyesmanya@gmail.com>
2026-04-06 15:02:50 -0300

    RED: property validation to keep the coverage

---
+++ DarkKitchen/DarkKitchen.Tests/Entities/UserTest.cs
@@ -0,0 +1,65 @@
+using DarkKitchen.Domain.Entities;
+
+namespace DarkKitchen.Tests.Entities;
+
+[TestClass]
+public class UserTest
+{
+    private readonly string name = "Name";
+    private readonly string surname = "Surname";
+    private readonly string email = "valid@email.com";
+    private readonly string phone = "+59899123123";
+    private readonly string password = "ValidPassword1!";
+    private readonly Role role = Role.Client;
+    private User? user;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        user = new User
+        {
+            Name = name,
+            Surname = surname,
+            Email = email,
+            Phone = phone,
+            Password = password,
+            Role = role
+        };
+    }
+
+    [TestMethod]
+    public void User_WithValidData_HasCorrectName()
+    {
+        Assert.AreEqual(name, user!.Name);
+    }
+
+    [TestMethod]
+    public void User_WithValidData_HasCorrectSurname()
+    {
+        Assert.AreEqual(surname, user!.Surname);
+    }
+
+    [TestMethod]
+    public void User_WithValidData_HasCorrectEmail()
+    {
+        Assert.AreEqual(email, user!.Email);
+    }
+
+    [TestMethod]
+    public void User_WithValidData_HasCorrectPhone()
+    {
+        Assert.AreEqual(phone, user!.Phone);
+    }
+
+    [TestMethod]
+    public void User_WithValidData_HasCorrectPassword()
+    {
+        Assert.AreEqual(password, user!.Password);
+    }
+
+    [TestMethod]
+    public void User_WithValidData_HasCorrectRole()
+    {
+        Assert.AreEqual(role, user!.Role);
+    }
+}
