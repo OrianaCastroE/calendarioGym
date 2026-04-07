3e4ad6ff9ed9d4a073e90f7fe2a6667aa72fc7bc
Juanicroquis <jc286049@fi365.ort.edu.uy>
2026-04-07 20:17:51 -0300

    CreatePromotion - GREEN

---
+++ DarkKitchen/DarkKitchen.Services/PromotionService.cs
@@ -0,0 +1,40 @@
+using DarkKitchen.Domain.DataAccess.Interfaces;
+using DarkKitchen.Domain.Entities;
+using DarkKitchen.Domain.Interfaces;
+using DarkKitchen.Models.PromotionDTOs;
+
+namespace DarkKitchen.Services;
+
+public class PromotionService(IPromotionRepository promotionRepository) : IPromotionService
+{
+    private readonly IPromotionRepository _promotionRepository = promotionRepository;
+
+    public void CreatePromotion(PromotionDto newPromotion)
+    {
+        var promotion = new Promotion()
+        {
+            Name = newPromotion.Name,
+            DiscountPercentage = newPromotion.DiscountPercentage,
+            DateFrom = newPromotion.DateFrom,
+            DateTo = newPromotion.DateTo
+        };
+
+        _promotionRepository.Add(promotion);
+        _promotionRepository.Save();
+    }
+
+    public void UpdatePromotion(int id, PromotionDto updatedPromotion)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdatePromotionProducts(int promotionId, List<int> productIds)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<PromotionResponseDto> GetPromotions(DateTime? date, string? productLine, string? productName)
+    {
+        throw new NotImplementedException();
+    }
+}
