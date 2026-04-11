117f9a83a7f44ac92932e3f307f714de4f2dfed3
Juanicroquis <jc286049@fi365.ort.edu.uy>
2026-04-11 17:42:40 -0300

    Add CreateOrder - GREEN

---
+++ DarkKitchen/DarkKitchen.Services/OrderService.cs
@@ -0,0 +1,59 @@
+using DarkKitchen.Domain.DataAccess.Interfaces;
+using DarkKitchen.Domain.Entities;
+using DarkKitchen.Domain.Interfaces;
+using DarkKitchen.Models.OrderDTOs;
+
+namespace DarkKitchen.Services;
+
+public class OrderService(
+    IOrderRepository orderRepository,
+    IProductRepository productRepository,
+    IPromotionRepository promotionRepository) : IOrderService
+{
+    private readonly IOrderRepository orderRepository = orderRepository;
+    private readonly IProductRepository productRepository = productRepository;
+    private readonly IPromotionRepository promotionRepository = promotionRepository;
+
+    public OrderResponseDto CreateOrder(OrderDto newOrder)
+    {
+        var order = new Order()
+        {
+            DeliveryType = newOrder.DeliveryType,
+            Street = newOrder.Address.Street,
+            DoorNumber = newOrder.Address.DoorNumber,
+            Apartment = newOrder.Address.Apartment,
+            Status = "Pending",
+            CreatedAt = DateTime.Now
+        };
+
+        orderRepository.Add(order);
+        orderRepository.Save();
+
+        return new OrderResponseDto()
+        {
+            Id = order.Id,
+            Status = order.Status,
+            CreatedAt = order.CreatedAt
+        };
+    }
+
+    public List<OrderResponseDto> GetClientOrders(int clientId, DateTime? dateFrom, DateTime? dateTo, string? status)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<OrderResponseDto> GetOrdersByStatus(DateTime dateFrom, DateTime dateTo, string? address, string? status)
+    {
+        throw new NotImplementedException();
+    }
+
+    public OrderResponseDto GetOrderById(int orderId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateOrderStatus(int orderId, UpdateOrderStatusDto newStatus)
+    {
+        throw new NotImplementedException();
+    }
+}
