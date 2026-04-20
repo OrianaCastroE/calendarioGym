23c332790ef62d71ceaeffba6eb48d8d483be388
Oriana Castro <OC251809@fi365.ort.edu.uy>
2026-04-20 17:36:50 -0300

    red: AddProduct_WhenProductIsValid_AddsProductToDatabase

---
+++ DarkKitchen/DarkKitchen.DataAccess/Repositories/ProductRepository.cs
@@ -0,0 +1,40 @@
+﻿using DarkKitchen.Domain.Entities;
+using DarkKitchen.Domain.Interfaces.Repository;
+using DarkKitchen.Models.DateDTOs;
+using DarkKitchen.Models.ProductDTOs;
+namespace DarkKitchen.DataAccess.Repositories;
+
+public class ProductRepository(AppDbContext context) : IProductRepository
+{
+    private readonly AppDbContext context = context;
+
+    public void Add(Product product)
+    {
+        return;
+    }
+
+    public void Update(Product product)
+    {
+        return;
+    }
+
+    public Product? GetById(int id)
+    {
+        return null;
+    }
+
+    public IEnumerable<Product> GetProducts(ProductFilterDto filter)
+    {
+        return null;
+    }
+
+    public IEnumerable<Product> GetMostRequestedProducts(DateRangeDto dates)
+    {
+        return null;
+    }
+
+    public void UpdateStatus(int id, ProductStatusDto status)
+    {
+        return;
+    }
+}
---
+++ DarkKitchen/DarkKitchen.Tests.DataAccess/ProductRepositoryTests.cs
@@ -0,0 +1,72 @@
+﻿using DarkKitchen.DataAccess;
+using DarkKitchen.DataAccess.Repositories;
+using DarkKitchen.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DarkKitchen.Tests.DataAccess;
+
+[TestClass]
+public class ProductRepositoryTests
+{
+    private Product? product;
+    private AppDbContext? context;
+    private ProductRepository? productRepository;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        product = new Product
+        {
+            Id = 1,
+            Code = "TEST001",
+            Name = "Test Product",
+            Description = "This is a test product.",
+            Category = "Test Category",
+            Price = 9.99m,
+            IsActive = true,
+            Images =
+            [
+                new ProductImage
+                {
+                    Id = 1,
+                    Url = "https://example.com/image1.jpg",
+                    ProductId = 1
+                },
+                new ProductImage
+                {
+                    Id = 2,
+                    Url = "https://example.com/image2.jpg",
+                    ProductId = 1
+                }
+
+            ]
+        };
+
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        context = new AppDbContext(options);
+        productRepository = new ProductRepository(context);
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        context!.Database.EnsureDeleted();
+        context.Dispose();
+    }
+
+    [TestMethod]
+    public void AddProduct_WhenProductIsValid_AddsProductToDatabase()
+    {
+        productRepository!.Add(product!);
+        context!.SaveChanges();
+
+        var result = context.Products.Include(p => p.Images).FirstOrDefault(p => p.Id == 1);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual("Test Product", result.Name);
+        Assert.AreEqual(2, result.Images.Count);
+    }
+}
