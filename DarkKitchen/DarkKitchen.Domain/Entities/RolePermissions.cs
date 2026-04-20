39dfde0eeb319fdb98c9a109b6226f3185cecf23
Emi1891 <emilianoreyesmanya@gmail.com>
2026-04-20 15:21:16 -0300

    Fix RolePermissions.cs to start with an empty list.

--- DarkKitchen/DarkKitchen.Domain/Entities/RolePermissions.cs
+++ DarkKitchen/DarkKitchen.Domain/Entities/RolePermissions.cs
@@ -5,5 +5,5 @@ namespace DarkKitchen.Domain.Entities;
 public class RolePermissions
 {
     public Role Role { get; set; }
-    public List<Permission> Permissions { get; set; }
+    public List<Permission> Permissions { get; set; } = [];
 }
