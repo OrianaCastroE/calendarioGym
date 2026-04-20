6c0b24f1747576dff2eb0855e0e49d58d97cb17f
Juanicroquis <jc286049@fi365.ort.edu.uy>
2026-04-19 23:19:17 -0300

    UpdateOrder - GREEN

--- DarkKitchen/DarkKitchen.DataAccess/Repositories/OrderRepository.cs
+++ DarkKitchen/DarkKitchen.DataAccess/Repositories/OrderRepository.cs
@@ -29,7 +29,14 @@ public class OrderRepository(AppDbContext context) : IOrderRepository
 
     public void Update(Order order)
     {
-        throw new NotImplementedException();
+        var existingOrder = context.Orders.Find(order.Id);
+        if(existingOrder == null)
+        {
+            throw new Exception("Order not found");
+        }
+
+        context.Entry(existingOrder).CurrentValues.SetValues(order);
+        context.SaveChanges();
     }
 
     public void Save()
