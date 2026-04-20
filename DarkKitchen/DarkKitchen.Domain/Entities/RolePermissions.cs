2c60c903e39efef6fbf8859a0c5d001d1d6228db
Emi1891 <emilianoreyesmanya@gmail.com>
2026-04-20 15:04:08 -0300

    RED: RolePermissions builder tests

---
+++ DarkKitchen/DarkKitchen.Domain/Entities/RolePermissions.cs
@@ -0,0 +1,9 @@
+﻿using DarkKitchen.Domain.Enums;
+
+namespace DarkKitchen.Domain.Entities;
+
+public class RolePermissions
+{
+    public Role Role { get; set; }
+    public List<Permission> Permissions { get; set; }
+}
