17fb4bcfa2d256da4a372dbce5e62c5eec5d8e6b
Juanicroquis <jc286049@fi365.ort.edu.uy>
2026-04-19 23:05:59 -0300

    Add OrderRepositoryTests - RED

---
+++ DarkKitchen/DarkKitchen.Tests.DataAccess/OrderRepositoryTests.cs
@@ -0,0 +1,44 @@
+using DarkKitchen.DataAccess;
+using DarkKitchen.DataAccess.Repositories;
+using DarkKitchen.Domain.Entities;
+using DarkKitchen.Domain.Interfaces.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace DarkKitchen.Tests.DataAccess;
+
+[TestClass]
+public class OrderRepositoryTests
+{
+    private Order? order;
+    private AppDbContext? context;
+    private OrderRepository? orderRepository;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        order = new Order
+        {
+            ClientId = 1,
+            DeliveryType = "express",
+            Street = "18 de Julio",
+            DoorNumber = "1234",
+            Apartment = "101",
+            Status = "Pending",
+            CreatedAt = DateTime.Now
+        };
+
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        context = new AppDbContext(options);
+        orderRepository = new OrderRepository(context);
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        context!.Database.EnsureDeleted();
+        context.Dispose();
+    }
+}
