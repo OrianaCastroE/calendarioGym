bcd9a24a49f91f32a39a4617a9d0c2e5b333b074
Juanicroquis <jc286049@fi365.ort.edu.uy>
2026-04-19 23:08:13 -0300

    Add OrderRepositoryTests - GREEN

---
+++ DarkKitchen/DarkKitchen.DataAccess/Repositories/OrderRepository.cs
@@ -0,0 +1,37 @@
+﻿using DarkKitchen.Domain.Entities;
+using DarkKitchen.Domain.Interfaces.Repository;
+
+namespace DarkKitchen.DataAccess.Repositories;
+
+public class OrderRepository(AppDbContext context) : IOrderRepository
+{
+    public void Add(Order order)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Order? GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Order> GetClientOrders(int clientId, DateTime? dateFrom, DateTime? dateTo, string? status)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Order> GetOrdersByStatus(DateTime dateFrom, DateTime dateTo, string? address, string? status)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Order order)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Save()
+    {
+        throw new NotImplementedException();
+    }
+}
