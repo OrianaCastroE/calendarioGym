5795b0e18b69ec24fb96763394790de416e92dab
Emi1891 <emilianoreyesmanya@gmail.com>
2026-04-24 20:15:42 -0300

    Added to the context the new entity RolePermission to be persisted and also added pre loaded RolePermissions inside OnModelCreating

--- DarkKitchen/DarkKitchen.DataAccess/AppDbContext.cs
+++ DarkKitchen/DarkKitchen.DataAccess/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using DarkKitchen.Domain.Entities;
+using DarkKitchen.Domain.Entities;
+using DarkKitchen.Domain.Enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace DarkKitchen.DataAccess;
@@ -11,4 +12,66 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
     public DbSet<ProductImage> ProductImages { get; set; }
     public DbSet<Promotion> Promotion { get; set; }
     public DbSet<User> Users { get; set; }
+    public DbSet<RolePermissions> RolePermissions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<RolePermissions>(b =>
+        {
+            b.HasKey(r => r.Role);
+
+            b.HasData(
+                new RolePermissions
+                {
+                    Role = Role.Admin,
+                    Permissions =
+                    [
+                        Permission.CreateUser,
+                        Permission.UpdateUser,
+                        Permission.DeleteUser,
+                        Permission.GetUsers,
+                        Permission.CreateProduct,
+                        Permission.UpdateProduct,
+                        Permission.GetProducts,
+                        Permission.CreatePromotion,
+                        Permission.UpdatePromotion,
+                        Permission.GetCurrentPromotions,
+                        Permission.GetOrderDetails,
+                        Permission.GetMostPopularProducts,
+                        Permission.GetSellsReport
+                    ]
+                },
+                new RolePermissions
+                {
+                    Role = Role.Client,
+                    Permissions =
+                    [
+                        Permission.GetProducts,
+                        Permission.GetCurrentPromotions,
+                        Permission.PlaceOrder,
+                        Permission.GetMyOrders
+                    ]
+                },
+                new RolePermissions
+                {
+                    Role = Role.Dispatcher,
+                    Permissions =
+                    [
+                        Permission.GetOrdersByStatus,
+                        Permission.GetOrderDetails,
+                        Permission.SetOrderStatusToPrepared,
+                        Permission.SetOrderStatusToCanceled,
+                        Permission.SetOrderStatusToOnItsWay,
+                        Permission.SetOrderStatusToDelivered,
+                        Permission.SetOrderStatusToNotDelivered
+                    ]
+                });
+        });
+
+        modelBuilder.Entity<User>()
+            .HasOne<RolePermissions>()
+            .WithMany()
+            .HasForeignKey(u => u.Role)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
