c9beb3dc4ea148655d1c6d364c303a899f422e0c
Juanicroquis <jc286049@fi365.ort.edu.uy>
2026-04-26 23:11:19 -0300

    StatusPendingToNotDelivered RED/GREEN

--- DarkKitchen/DarkKitchen.Tests.Services/OrderServiceTest.cs
+++ DarkKitchen/DarkKitchen.Tests.Services/OrderServiceTest.cs
@@ -242,4 +242,13 @@ public class OrderServiceTest
         Assert.ThrowsException<BadRequestException>(() =>
             orderService!.UpdateOrderStatus(1, new UpdateOrderStatusDto(nameof(OrderStatus.Delivered)), [Permission.SetOrderStatusToDelivered]));
     }
+
+    [TestMethod]
+    public void UpdateOrderStatus_FromPendingToNotDelivered_ThrowsBadRequestException()
+    {
+        orderRepositoryMock!.Setup(r => r.GetById(1)).Returns(orderEntity!);
+
+        Assert.ThrowsException<BadRequestException>(() =>
+            orderService!.UpdateOrderStatus(1, new UpdateOrderStatusDto(nameof(OrderStatus.NotDelivered)), [Permission.SetOrderStatusToNotDelivered]));
+    }
 }
