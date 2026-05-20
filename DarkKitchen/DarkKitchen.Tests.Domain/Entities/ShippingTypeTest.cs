abbfee992f22908f66cd2c8af6c31b800e8f69be
Oriana Castro <OC251809@fi365.ort.edu.uy>
2026-05-20 20:13:25 -0300

    red: ShippingType_WithValidData_CreatedCorrectly

---
+++ DarkKitchen/DarkKitchen.Tests.Domain/Entities/ShippingTypeTest.cs
@@ -0,0 +1,20 @@
+using DarkKitchen.Domain.Entities;
+using DarkKitchen.Domain.Exceptions;
+
+namespace DarkKitchen.Tests.Domain.Entities;
+
+[TestClass]
+public class ShippingTypeTest
+{
+    private readonly string validName = "Express";
+    private readonly decimal validPrice = 250;
+
+    [TestMethod]
+    public void ShippingType_WithValidData_CreatedCorrectly()
+    {
+        var shippingType = new ShippingType(validName, validPrice);
+
+        Assert.AreEqual(validName, shippingType.Name);
+        Assert.AreEqual(validPrice, shippingType.Price);
+    }
+}
