76c592ca3f16ecd6e0c4cb33a1fb68054f8fae3d
Oriana Castro <OC251809@fi365.ort.edu.uy>
2026-05-20 23:17:56 -0300

    green: Add_WhenShippingTypeIsValid_AddsToDatabase

---
+++ DarkKitchen/DarkKitchen.Tests.DataAccess/ShippingTypeRepositoryTests.cs
@@ -0,0 +1,46 @@
+using DarkKitchen.DataAccess;
+using DarkKitchen.DataAccess.Repositories;
+using DarkKitchen.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DarkKitchen.Tests.DataAccess;
+
+[TestClass]
+public class ShippingTypeRepositoryTests
+{
+    private ShippingType? shippingType;
+    private AppDbContext? context;
+    private ShippingTypeRepository? shippingTypeRepository;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        shippingType = new ShippingType { Name = "Express", Price = 250 };
+
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        context = new AppDbContext(options);
+        shippingTypeRepository = new ShippingTypeRepository(context);
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        context!.Database.EnsureDeleted();
+        context.Dispose();
+    }
+
+    [TestMethod]
+    public void Add_WhenShippingTypeIsValid_AddsToDatabase()
+    {
+        shippingTypeRepository!.Add(shippingType!);
+
+        var result = context!.ShippingTypes.FirstOrDefault(s => s.Name == "Express");
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual("Express", result.Name);
+        Assert.AreEqual(250, result.Price);
+    }
+}
