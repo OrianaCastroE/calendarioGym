ff6e52b53aa95f3cb5f140b2975d433ed4f838cc
Oriana Castro <OC251809@fi365.ort.edu.uy>
2026-05-22 18:56:00 -0300

    fix: clean code

--- DarkKitchen/DarkKitchen.DataAccess/Repositories/ShippingTypeRepository.cs
+++ DarkKitchen/DarkKitchen.DataAccess/Repositories/ShippingTypeRepository.cs
@@ -24,7 +24,11 @@ public class ShippingTypeRepository(AppDbContext context) : IShippingTypeReposit
     public void Update(ShippingType shippingType)
     {
         var existing = context.ShippingTypes.Find(shippingType.Id);
-        if(existing is null) return;
+        if(existing is null)
+        {
+            return;
+        }
+
         context.Entry(existing).CurrentValues.SetValues(shippingType);
         context.SaveChanges();
     }
