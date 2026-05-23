040fd4bbcf0bf2b16e84fb80ce04b7db4efbbbd4
Emi1891 <emilianoreyesmanya@gmail.com>
2026-05-23 03:54:51 -0300

    Importer registry tests

---
+++ DarkKitchen/DarkKitchen.Tests.Services/Plugins/ImporterRegistryTest.cs
@@ -0,0 +1,96 @@
+using DarkKitchen.Domain.Exceptions;
+using DarkKitchen.Domain.Interfaces.Service;
+using DarkKitchen.Models.ProductDTOs;
+using DarkKitchen.Services.Plugins;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace DarkKitchen.Tests.Services.Plugins;
+
+[TestClass]
+public class ImporterRegistryTest
+{
+    private Mock<PluginLoader>? loaderMock;
+    private Mock<IConfiguration>? configurationMock;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        loaderMock = new Mock<PluginLoader>();
+        loaderMock.Setup(l => l.LoadFrom(It.IsAny<string>())).Returns(new List<IProductImporter>());
+
+        configurationMock = new Mock<IConfiguration>();
+        configurationMock.Setup(c => c["Plugins:Folder"]).Returns("test-plugins");
+    }
+
+    [TestMethod]
+    public void GetAll_WhenBuiltInsRegistered_ReturnsAll()
+    {
+        var json = new FakeImporter("JSON", ".json");
+        var xml = new FakeImporter("XML", ".xml");
+        var registry = new ImporterRegistry([json, xml], loaderMock!.Object, configurationMock!.Object);
+
+        var all = registry.GetAll().ToList();
+
+        Assert.AreEqual(2, all.Count);
+    }
+
+    [TestMethod]
+    public void Get_WhenNameMatches_ReturnsImporter()
+    {
+        var json = new FakeImporter("JSON", ".json");
+        var registry = new ImporterRegistry([json], loaderMock!.Object, configurationMock!.Object);
+
+        var result = registry.Get("JSON");
+
+        Assert.AreSame(json, result);
+    }
+
+    [TestMethod]
+    public void Get_WhenNameIsCaseInsensitive_ReturnsImporter()
+    {
+        var json = new FakeImporter("JSON", ".json");
+        var registry = new ImporterRegistry([json], loaderMock!.Object, configurationMock!.Object);
+
+        var result = registry.Get("json");
+
+        Assert.AreSame(json, result);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(NotFoundException))]
+    public void Get_WhenNameUnknown_ThrowsNotFound()
+    {
+        var registry = new ImporterRegistry([], loaderMock!.Object, configurationMock!.Object);
+
+        registry.Get("Unknown");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void Constructor_WhenDuplicateBuiltInNames_Throws()
+    {
+        var a = new FakeImporter("JSON", ".json");
+        var b = new FakeImporter("JSON", ".json");
+
+        _ = new ImporterRegistry([a, b], loaderMock!.Object, configurationMock!.Object);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void Constructor_WhenPluginNameCollidesWithBuiltIn_Throws()
+    {
+        var builtIn = new FakeImporter("JSON", ".json");
+        var plugin = new FakeImporter("JSON", ".json");
+        loaderMock!.Setup(l => l.LoadFrom(It.IsAny<string>())).Returns(new List<IProductImporter> { plugin });
+
+        _ = new ImporterRegistry([builtIn], loaderMock.Object, configurationMock!.Object);
+    }
+
+    private sealed class FakeImporter(string name, string extension) : IProductImporter
+    {
+        public string Name { get; } = name;
+        public string Extension { get; } = extension;
+        public IEnumerable<ImportedProductDto> Import(Stream source) => [];
+    }
+}
