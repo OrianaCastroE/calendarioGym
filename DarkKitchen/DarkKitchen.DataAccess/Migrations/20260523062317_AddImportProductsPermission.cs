4c9a1a952d4b088da3a7c4d67b1b40314a567257
Emi1891 <emilianoreyesmanya@gmail.com>
2026-05-23 03:57:42 -0300

    New migration appling new changes

---
+++ DarkKitchen/DarkKitchen.DataAccess/Migrations/20260523062317_AddImportProductsPermission.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DarkKitchen.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImportProductsPermission : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "RolePermissions",
+                keyColumn: "Role",
+                keyValue: 0,
+                column: "Permissions",
+                value: "[3,4,5,6,7,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "RolePermissions",
+                keyColumn: "Role",
+                keyValue: 0,
+                column: "Permissions",
+                value: "[3,4,5,6,7,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]");
+        }
+    }
+}
