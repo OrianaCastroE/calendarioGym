1ab30f362dc6660197c36a3ad277eb1303c6b5f2
Juanicroquis <jc286049@fi365.ort.edu.uy>
2026-06-08 19:15:41 -0300

    Audit UI and correction in edit product

--- DarkKitchenUI/src/app/shared/navbar/navbar.component.ts
+++ DarkKitchenUI/src/app/shared/navbar/navbar.component.ts
@@ -56,7 +56,8 @@ export class NavbarComponent {
     {
       label: 'Audit',
       route: '/audit',
-      icon: 'M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4z'
+      icon: 'M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4z',
+      permission: 'GetAuditRecords'
     },
     {
       label: 'Sales Report',
